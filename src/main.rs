40ff3e79a758e1167777f31ee79a536cc86e9236
Simon Gasse <sgasse@users.noreply.github.com>
2021-10-27 23:16:52 +0200

    Rotate text and implement function to find min/max positions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use image::{open as open_image, Rgb, RgbImage};
-use imageproc::{
-    definitions::Image,
-    drawing::{draw_cross, draw_text},
-    geometric_transformations::{warp, Interpolation, Projection},
+use image::{
+    imageops::{overlay, rotate90},
+    open as open_image, Rgba, RgbaImage,
 };
+use imageproc::{definitions::Image, drawing::draw_text};
 use rusttype::{Font, Scale};
 
 fn main() {
@@ -14,20 +13,22 @@ fn main() {
 }
 
 fn process_main() -> Result<(), String> {
-    let img = read_and_decode("test_in.png")?;
+    // let img = read_and_decode("test_in.png")?;
+    let img = RgbaImage::from_pixel(400, 400, Rgba([0, 0, 0, 0]));
     let img = transform(img)?;
     write_image(img, "test_out.png")?;
     Ok(())
 }
 
-fn read_and_decode(img_file: &str) -> Result<RgbImage, String> {
+#[allow(dead_code)]
+fn read_and_decode(img_file: &str) -> Result<RgbaImage, String> {
     match open_image(img_file) {
-        Ok(img) => Ok(img.into_rgb8()),
+        Ok(img) => Ok(img.into_rgba8()),
         Err(_) => Err(format!("Could not read {}", img_file)),
     }
 }
 
-fn write_image(img: Image<Rgb<u8>>, filename: &str) -> Result<(), String> {
+fn write_image(img: Image<Rgba<u8>>, filename: &str) -> Result<(), String> {
     match img.save(filename) {
         Ok(_) => {
             println!("Wrote file {}", filename);
@@ -45,17 +46,45 @@ fn load_font() -> Font<'static> {
     return font;
 }
 
-fn transform(mut img: Image<Rgb<u8>>) -> Result<Image<Rgb<u8>>, String> {
+fn find_bbox(img: &Image<Rgba<u8>>) -> (u32, u32, u32, u32) {
+    let mut xmin = 400;
+    let mut xmax = 0;
+    let mut ymin = 400;
+    let mut ymax = 0;
+
+    for (x, y, pixel) in img.enumerate_pixels() {
+        if *pixel != Rgba([0, 0, 0, 0]) {
+            if x < xmin {
+                xmin = x;
+            }
+            if y < ymin {
+                ymin = y
+            }
+            if x > xmax {
+                xmax = x;
+            }
+            if y > ymax {
+                ymax = y;
+            }
+        }
+    }
+
+    (xmin, xmax, ymin, ymax)
+}
+
+fn transform(mut img: Image<Rgba<u8>>) -> Result<Image<Rgba<u8>>, String> {
     img = draw_text(
         &mut img,
-        Rgb([50, 255, 50]),
+        Rgba([1, 0, 0, 255]),
         20,
-        20,
-        Scale { x: 5.0, y: 80.0 },
+        0,
+        Scale { x: 20.0, y: 400.0 },
         &load_font(),
-        "This is a very long text let's write it",
+        "Hummus aus Moosach",
     );
-    let trans_fn = Projection::scale(1.0, 1.0);
-    let img = warp(&img, &trans_fn, Interpolation::Bilinear, Rgb([0, 0, 0]));
+    let bbox = find_bbox(&img);
+    let img_rotated = rotate90(&img);
+
+    overlay(&mut img, &img_rotated, 0, 0);
     return Ok(img);
 }
