0847dabe10fc91d69c4bbba2fe2bfc662c5f08d6
Simon Gasse <sgasse@users.noreply.github.com>
2021-10-27 23:56:48 +0200

    Overlay with rotated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use image::{
     imageops::{crop, overlay, rotate90},
-    open as open_image, GenericImage, GenericImageView, Rgba, RgbaImage, SubImage,
+    open as open_image, GenericImage, GenericImageView, Rgba, RgbaImage,
 };
 use imageproc::{definitions::Image, drawing::draw_text};
 use rusttype::{Font, Scale};
@@ -13,9 +13,8 @@ fn main() {
 }
 
 fn process_main() -> Result<(), String> {
-    // let img = read_and_decode("test_in.png")?;
-    let img = RgbaImage::from_pixel(400, 400, Rgba([0, 0, 0, 0]));
-    let img = transform(img)?;
+    let img = get_scaled_cropped_text("Hummus aus Moosach");
+    let img = overlay_with_rotated(img);
     write_image(img, "test_out.png")?;
     Ok(())
 }
@@ -47,9 +46,10 @@ fn load_font() -> Font<'static> {
 }
 
 fn find_bbox(img: &Image<Rgba<u8>>) -> (u32, u32, u32, u32) {
-    let mut xmin = 400;
+    let (width, height) = img.dimensions();
+    let mut xmin = width;
     let mut xmax = 0;
-    let mut ymin = 400;
+    let mut ymin = height;
     let mut ymax = 0;
 
     for (x, y, pixel) in img.enumerate_pixels() {
@@ -72,7 +72,8 @@ fn find_bbox(img: &Image<Rgba<u8>>) -> (u32, u32, u32, u32) {
     (xmin, xmax, ymin, ymax)
 }
 
-fn transform(mut img: Image<Rgba<u8>>) -> Result<Image<Rgba<u8>>, String> {
+fn get_scaled_cropped_text(text: &str) -> Image<Rgba<u8>> {
+    let mut img = RgbaImage::from_pixel(400, 400, Rgba([0, 0, 0, 0]));
     img = draw_text(
         &mut img,
         Rgba([0, 0, 0, 255]),
@@ -80,14 +81,35 @@ fn transform(mut img: Image<Rgba<u8>>) -> Result<Image<Rgba<u8>>, String> {
         0,
         Scale { x: 20.0, y: 400.0 },
         &load_font(),
-        "Hummus aus Moosach",
+        text,
     );
     let bbox = find_bbox(&img);
     let width = bbox.1 - bbox.0;
     let height = bbox.3 - bbox.2;
-    let new_img = crop(&mut img.clone(), bbox.0, bbox.2, width, height).to_image();
-    let img_rotated = rotate90(&img);
+    crop(&mut img, bbox.0, bbox.2, width, height).to_image()
+}
 
-    overlay(&mut img, &img_rotated, 0, 0);
-    return Ok(new_img);
+fn overlay_with_rotated(img: Image<Rgba<u8>>) -> Image<Rgba<u8>> {
+    let (width, height) = img.dimensions();
+    let length = u32::max(width, height) + 10;
+
+    let rotated = rotate90(&img.clone());
+
+    let mut bottom = RgbaImage::from_pixel(length, length, Rgba([0, 0, 0, 0]));
+
+    overlay(
+        &mut bottom,
+        &img,
+        (length - width) / 2,
+        (length - height) / 2,
+    );
+
+    overlay(
+        &mut bottom,
+        &rotated,
+        // inverted for rotated image
+        (length - height) / 2,
+        (length - width) / 2,
+    );
+    bottom
 }
