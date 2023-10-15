5ea4c7b84938f4d0848a5be50fe00f563a46d8ee
Simon B. Gasse <simon.gasse@accenture.com>
2023-10-15 09:57:06 +0200

    Bump dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,9 @@ use frontend::setup_input_onchange_callback;
 #[wasm_bindgen]
 pub fn wasm_main() {
     set_panic_hook();
+
     let canvas_size = get_min_window_dim() as u32;
     set_canvas_size(canvas_size, canvas_size);
+
     setup_input_onchange_callback(500);
 }
